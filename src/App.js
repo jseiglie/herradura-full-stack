6404b11c3b0e9e28a111993022cc3678b525bcd8
Javier Leyva Seiglie <96433186+jseiglie@users.noreply.github.com>
2022-08-27 16:35:32 +0200

    develop branch created

--- src/App.js
+++ src/App.js
@@ -1,14 +1,25 @@
-
+import { Route, Routes } from "react-router";
+import { BrowserRouter } from "react-router-dom";
+import FooterH from "./Components/FooterH";
+import NavBar from "./Components/NavBar";
 import "./Styles/App.css";
+import Admin from "./Views/Admin";
 import Checkout from "./Views/Checkout";
+import Home from "./Views/Home";
 
 function App() {
-
   return (
     <div className="App">
-      <Checkout />
-
-    </div> 
+      <NavBar />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/checkout" element={<Checkout />} />
+          <Route path="/admin" element={<Admin />} />
+        </Routes>
+      </BrowserRouter>
+      <FooterH />
+    </div>
   );
 }
 
