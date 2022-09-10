a1b0dc5c91b18053d430b6ce0cf86f82724d4e1b
Javier Leyva Seiglie <96433186+jseiglie@users.noreply.github.com>
2022-09-10 02:19:10 +0200

    select in form

--- src/Views/Edit.js
+++ src/Views/Edit.js
@@ -21,7 +21,6 @@ const Edit = () => {
           `${process.env.REACT_APP_APIURL}/oneItem/${id}`
         );
         setData(res.data);
-        
       } catch (error) {
         console.log(error);
       }
@@ -40,51 +39,74 @@ const Edit = () => {
     <div>
       <h1>Añadir/Editar</h1>
 
-      <div className="container">
+      <div className="container d-flex">
         <form className="form-control" onSubmit={(e) => handleSubmit(e)}>
-          <label htmlFor="plato">Plato</label>
-          <input
-            className="form-control"
-            id="plato"
-            type="text"
-            placeholder={data.plato}
-            name="plato"
-            onChange={(e) => setPlato(e.target.value)}
-          />
-          <label htmlFor="descripcion">Descripcion</label>
-          <input
-            className="form-control"
-            id="descripcion"
-            type="text"
-            placeholder={data.descripcion}
-            name="descripcion"
-            onChange={(e) => setDescripcion(e.target.value)}
-          />
-          <label htmlFor="precio">Precio</label>
-          <input
-            className="form-control"
-            id="precio"
-            type="number"
-            placeholder={data.precio}
-            name="precio"
-            onChange={(e) => setPrecio(e.target.value)}
-          />
-          <label htmlFor="vegano">Vegano</label>
-          ----options-----
-          <label htmlFor="categoria">Categoria</label>
-          <input
-            className="form-control"
-            id="categoria"
-            type="text"
-            placeholder={data.id_catego2}
-            name="categoria"
-            onChange={(e) => setId_catego2(e.target.value)}
-          />
-          <label htmlFor="disponible">Disponible</label>
-          ----options-----
-          <label htmlFor="destacar">Destacar</label>
-          ----options-----
-          <input className="form-control" type="submit" value={"enviar"} />
+          <div className="row">
+            <div className="col">
+              <label htmlFor="plato">Plato</label>
+              <input
+                className="form-control"
+                id="plato"
+                type="text"
+                placeholder={data.plato}
+                name="plato"
+                onChange={(e) => setPlato(e.target.value)}
+              />
+              <label htmlFor="descripcion">Descripcion</label>
+              <input
+                className="form-control"
+                id="descripcion"
+                type="text"
+                placeholder={data.descripcion}
+                name="descripcion"
+                onChange={(e) => setDescripcion(e.target.value)}
+              />
+              <label htmlFor="precio">Precio</label>
+              <input
+                className="form-control"
+                id="precio"
+                type="number"
+                placeholder={data.precio}
+                name="precio"
+                onChange={(e) => setPrecio(e.target.value)}
+              />
+              <label htmlFor="categoria">Categoria</label>
+              <input
+                className="form-control"
+                id="categoria"
+                type="text"
+                placeholder={data.id_catego2}
+                name="categoria"
+                onChange={(e) => setId_catego2(e.target.value)}
+              />
+            </div>
+            <div className="col">
+              <label htmlFor="vegano">Vegano</label>
+              <select id="vegano" name="vegano">
+                <option value={true}>Si</option>
+                <option value={false} selected>
+                  No
+                </option>
+              </select>
+              <br />
+              <label htmlFor="disponible">Disponible</label>
+              <select id="disponible" name="disponible">
+                <option value={true} selected>
+                  Si
+                </option>
+                <option value={false}>No</option>
+              </select>
+              <br />
+              <label htmlFor="destacar">Destacar</label>
+              <select id="destacar" name="destacar">
+                <option value={true}>Si</option>
+                <option value={false} selected>
+                  No
+                </option>
+              </select>
+              <input className="form-control" type="submit" value={"enviar"} />
+            </div>
+          </div>
         </form>
       </div>
     </div>
